8f2aa2e4d288db303a82df5a2382c392da574ab1
xinzhang <xinzhang@thoughtworks.com>
2017-07-29 16:03:53 +0800

    [Xin] Add basic product component

--- src/store/routers.tsx
+++ src/store/routers.tsx
@@ -3,11 +3,13 @@ import { Route } from 'react-router';
 
 import HomePage from '../containers/pages/Home/HomePage';
 import AboutUsPage from '../containers/pages/AboutUs/AboutUsPage';
+import {Product} from '../components/product/Product';
 
 const router = (
     <div>
         <Route exact={true} path="/" component={HomePage}/>
         <Route path="/about-us" component={AboutUsPage}/>
+        <Route path="/product" component={Product}/>
     </div>
 );
 
